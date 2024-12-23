fabf7bc1fd48bca0d3b6ceebbe72aa8f81fdd18f
Aleks <fenenovicaleks@gmail.com>
2024-12-23 22:36:23 +0500

    Update final.py

--- final.py
+++ final.py
@@ -1,29 +1,36 @@
 from datetime import datetime
 
-username = input("Введите Имя пользователя:")
+username = input("Введите Имя пользователя: ")
 title1 = input("Введите первый заголовок: ")
 title2 = input("Введите второй заголовок: ")
 title3 = input("Введите третий заголовок: ")
 content = input("Введите Описание заметки:")
-status = input("Введите Статус заметки:")
-created_date = input("Введите Дата создания заметки:")
-issue_date = input("Введите Дата истечения заметки (дедлайн):")
-
+status = input("Введите Статус заметки: ")
+created_date = input("Введите Дата создания заметки (в формате ДД-ММ-ГГ): ")
+issue_date = input("Введите Дата истечения заметки (дедлайн), (в формате ДД-ММ-ГГ): ")
 title_list = [title1, title2, title3]
-note = []
-note.extend([username, content, status, created_date, issue_date, title_list])
+
+note = {}
+note['Имя пользователя: '] = username
+note['Описание заметки: '] = content
+note['Статус заметки: '] = status
+note['Дата создания заметки: '] = created_date
+note['Дата истечения заметки (дедлайн): '] = issue_date
+note['Заголовки: '] = title_list
+
+print("\nЗаметка:")
+
+for key, value in note.items():
+    print(f"{key}: {value}")
 
 def format_date_without_year(date_str):
     date_obj = datetime.strptime(date_str, "%d-%m-%Y")
     return date_obj.strftime("%d-%m")
 
-print('\nИнформация о заметке:')
-print(f"Имя пользователя: {note[0]}")
-print(f"Содержание заметки: {note[1]}")
-print(f"Статус заметки: {note[2]}")
-print(f"Дата создания заметки: ", format_date_without_year(created_date))
-print(f"Дата истечения заметки (дедлайн): ", format_date_without_year(issue_date))
-print(f"Заголовки: ")
+
+
+
+
 
 for index, title_list in enumerate(note[5], start=1):
     print(f"\tЗаголовок {index}: {title_list}")
